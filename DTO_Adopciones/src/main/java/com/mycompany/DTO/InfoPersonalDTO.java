95eee12c88d25b7948acca8893851fcf6ccf8220
josebeltran26 <jose.beltran262119@potros.itson.edu.mx>
2025-10-29 09:28:58 -0700

    Dto

---
+++ DTO_Adopciones/src/main/java/com/mycompany/DTO/InfoPersonalDTO.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.DTO;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author Josel
+ */
+
+
+public class InfoPersonalDTO {
+
+    private String nombre;
+    private int edad;
+    private String direccion;
+    private Image imagenIdentificacion;
+    private String sexo; 
+    private String estatusEmpleo; 
+    private double salarioMensual; 
+
+    public InfoPersonalDTO() {
+    }
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Image getImagenIdentificacion() {
+        return imagenIdentificacion;
+    }
+
+    public void setImagenIdentificacion(Image imagenIdentificacion) {
+        this.imagenIdentificacion = imagenIdentificacion;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEstatusEmpleo() {
+        return estatusEmpleo;
+    }
+
+    public void setEstatusEmpleo(String estatusEmpleo) {
+        this.estatusEmpleo = estatusEmpleo;
+    }
+
+    public double getSalarioMensual() {
+        return salarioMensual;
+    }
+
+    public void setSalarioMensual(double salarioMensual) {
+        this.salarioMensual = salarioMensual;
+    }
+}
